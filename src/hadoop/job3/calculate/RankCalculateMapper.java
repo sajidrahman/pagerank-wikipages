9238c9fa61e5b220f7e479ef25236e15757c448f
sajidrahman <sajidrahman25@gmail.com>
2016-11-30 08:49:17 -0600

    -update calculation - get corpus-size from config

---
+++ src/hadoop/job3/calculate/RankCalculateMapper.java
@@ -0,0 +1,49 @@
+package hadoop.job3.calculate;
+
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class RankCalculateMapper extends Mapper<LongWritable, Text, Text, Text> {
+
+	
+    @Override
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException { //TODO: mess with
+    	
+    	// Gets the string of the value 
+    	String valueStr = value.toString();
+    	String[] sections = valueStr.split("\\t");
+
+    	// Gets the mapped page
+        String mappedPage = sections[0];
+        
+        // Gets the [thisPage]	[thisPagesRank]
+        String mappedPageStr = sections[0] + "\t" + sections[1] + "\t";
+        
+        // Ignore if page contains no links
+        if(sections.length < 3 || sections[2] == "")
+        {
+        	// this is a sink-page
+        	context.write(new Text(mappedPage), new Text("#"));
+        	return;
+        }
+
+        // Get the linked pages and total no. of outgoing links of the source
+        String linksInPages = sections[2];
+        String[] linkedPages = linksInPages.split(";");
+        int total = linkedPages.length;
+ 
+        // For each linked-page, store [linked-Page]    [sourcePageRank]    [totalNumberOfOutgoingLinksOfSource]
+        for (String page : linkedPages) {
+            context.write(new Text(page), new Text(mappedPageStr + total));
+        }
+ 
+        // Adds original links for preservation
+        context.write(new Text(mappedPage), new Text("|" + linksInPages));
+    
+    }
+    
+}
---
+++ src/hadoop/job3/calculate/RankCalculateReducer.java
@@ -0,0 +1,59 @@
+package hadoop.job3.calculate;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class RankCalculateReducer extends Reducer<Text, Text, Text, Text> {
+    
+    @Override
+    public void reduce(Text page, Iterable<Text> values, Context context) throws IOException, InterruptedException { //TODO: mess with
+
+      Configuration config = context.getConfiguration();
+      Integer N = Integer.parseInt(config.get("size"));
+    	double d = 0.85;
+    	boolean isSinkPage = false;
+        double rankVote = 0.0;
+        String links = "";
+        Iterator<Text> val = values.iterator();
+        
+        // For each page that links to the current
+        while (val.hasNext()) {
+        	String pageStr = val.next().toString();
+ 
+        	// If it is a links record, add it to the links array
+            if (pageStr.startsWith("|")) {
+                links = pageStr.substring(1);
+            }else if(pageStr.equals("#")){
+            	isSinkPage = true;
+            }
+            // If it is a normal record however
+            else {
+            // Find the pagerank and number of links for the given page
+            String[] sections = pageStr.split("\\t");
+            double currentPageRank = Double.valueOf(sections[1]);
+            int outboundLinkCount = Integer.valueOf(sections[2]);
+ 
+            // Add the given pagerank to the running total for the other pages
+            rankVote += currentPageRank / outboundLinkCount;
+            }
+        }
+
+        Double pageRank = (1-d)/N + d * rankVote ;
+
+       if(isSinkPage){
+    	   context.write(page, new Text(pageRank.toString()));
+       }else{
+    	   context.write(page, new Text(pageRank.toString() + "\t" + links));
+       }
+
+       
+    }
+
+    
+}
